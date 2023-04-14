63a0b55aee507587dc9b222a433f3d96d1aff9b6
Huseyin Ahmetoglu <huseyinahmetoglu@artuklu.edu.tr>
2023-04-14 15:35:31 +0300

    Commit

--- src/main/java/com/huseyinahmetoglu/hoaxify/controller/UserController.java
+++ src/main/java/com/huseyinahmetoglu/hoaxify/controller/UserController.java
@@ -1,16 +1,20 @@
 package com.huseyinahmetoglu.hoaxify.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import com.huseyinahmetoglu.hoaxify.entity.User;
+import com.huseyinahmetoglu.hoaxify.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
 
-    Logger logger = LoggerFactory.getLogger(UserController.class);
+    private final UserService userService;
 
     @PostMapping("/api/1.0/users")
-    public void create(@RequestBody String body) {
-        logger.info(body);
+    public void create(@RequestBody User user) {
+        userService.save(user);
     }
 }
---
+++ src/main/java/com/huseyinahmetoglu/hoaxify/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.huseyinahmetoglu.hoaxify.repository;
+
+import com.huseyinahmetoglu.hoaxify.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
