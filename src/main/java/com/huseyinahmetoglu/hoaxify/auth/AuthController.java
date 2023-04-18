73acc6cf65fc78b9886f62470cc2ff5902091cff
Huseyin Ahmetoglu <huseyinahmetoglu@artuklu.edu.tr>
2023-04-18 20:06:18 +0300

    Commit50

--- src/main/java/com/huseyinahmetoglu/hoaxify/auth/AuthController.java
+++ src/main/java/com/huseyinahmetoglu/hoaxify/auth/AuthController.java
@@ -1,12 +1,9 @@
 package com.huseyinahmetoglu.hoaxify.auth;
 
-import com.huseyinahmetoglu.hoaxify.error.ApiError;
 import com.huseyinahmetoglu.hoaxify.user.User;
 import com.huseyinahmetoglu.hoaxify.user.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,32 +15,14 @@ import java.util.Base64;
 public class AuthController {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-
 
     @PostMapping("/api/1.0/auth")
-    ResponseEntity<?> handleAuthentication(@RequestHeader(name = "Authorization", required = false) String authorization) {
-        if (authorization == null) {
-            ApiError apiError = new ApiError(401, "Unauthorized request!", "/api/1.0/auth");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(apiError);
-        }
+    ResponseEntity<?> handleAuthentication(@RequestHeader(name = "Authorization") String authorization) {
         String base64encoded = authorization.split("Basic ")[1];
         String decoded = new String(Base64.getDecoder().decode(base64encoded));
         String[] parts = decoded.split(":");
         String username = parts[0];
-        String password = parts[1];
         User inDB = userRepository.findByUsername(username);
-        if (inDB == null) {
-            ApiError apiError = new ApiError(401, "Unauthorized request!", "/api/1.0/auth");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(apiError);
-        }
-        String hashedPassword = inDB.getPassword();
-        if (!passwordEncoder.matches(password, hashedPassword)) {
-            ApiError apiError = new ApiError(401, "Unauthorized request!", "/api/1.0/auth");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(apiError);
-        }
-
-
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(inDB);
     }
 }
--- src/main/java/com/huseyinahmetoglu/hoaxify/user/UserController.java
+++ src/main/java/com/huseyinahmetoglu/hoaxify/user/UserController.java
@@ -1,16 +1,11 @@
 package com.huseyinahmetoglu.hoaxify.user;
 
-import com.huseyinahmetoglu.hoaxify.error.ApiError;
 import com.huseyinahmetoglu.hoaxify.shared.GenericResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,15 +19,4 @@ public class UserController {
         return new GenericResponse("User created!");
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiError handleValidationException(MethodArgumentNotValidException exception) {
-        ApiError apiError = new ApiError(400, "Validation Error", "/api/1.0/users");
-        Map<String, String> validationErrors = new HashMap<>();
-        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
-            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
-        }
-        apiError.setValidationErrors(validationErrors);
-        return apiError;
-    }
 }
